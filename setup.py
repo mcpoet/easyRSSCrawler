6133603a7e83025578b1aed1c03479b6acc81022
mcpoet <marrowsky@gmail.com>
2013-09-06 15:54:14 +0800

    setup back using Distribute

--- setup.py
+++ setup.py
@@ -1,33 +1,22 @@
-PACKAGE = ""
-NAME = ""
-DESCRIPTION = ""
-AUTHOR = ""
-AUTHOR_EMAIL = ""
-URL = ""
-VERSION = __import__(PACKAGE).__version__
+PACKAGE = "easycrawler"
+NAME = "easy_crawler"
+DESCRIPTION = "very easy crawler in python"
+AUTHOR = "mcpoet"
+AUTHOR_EMAIL = "marrowsky@gmail.com"
+URL = "http://www.baidu.com"
+VERSION = 1.0
 
+from setuptools import setup, find_packages
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.rst"),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="BSD",
-    url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(
 			PACKAGE,
-			only_in_packages=False
-	  ),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Framework :: Django",
-    ],
+			only_in_packages=False),
     zip_safe=False,
 )
